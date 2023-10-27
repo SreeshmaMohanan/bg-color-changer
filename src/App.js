1acd849d56435c7bb38226b01f1ead978f74a075
SreeshmaMohanan <ppsreeshma10@mail.com>
2023-10-27 15:10:48 +0530

    color-changer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const change1=()=>{
+    document.body.style.background='blue'
+  }
+  const change2=()=>{
+    document.body.style.background='green'
+  }
+  const change3=()=>{
+    document.body.style.background='red'
+  }
+  const change4=()=>{
+    document.body.style.background='yellow'
+  }
+  const change5=()=>{
+    document.body.style.background='orange'
+  }
+  const change6=()=>{
+    document.body.style.background='purple'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+       <h1>Background Colour Changer</h1>
+      <div className='btns'>
+        <button className='btn' onClick={change1}>blue</button>
+        <button className='btn' onClick={change2}>Green</button>
+        <button className='btn'onClick={change3}>Red</button>
+        <button className='btn' onClick={change4}>Yellow</button>
+        <button className='btn' onClick={change5}>Orange</button>
+        <button className='btn' onClick={change6}>Purple</button>
+      </div>
+      </div>
+     
     </div>
   );
-}
+  }
 
 export default App;
